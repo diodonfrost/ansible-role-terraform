a72259897d34e8b2023804a19a9c468e1a63e17a
diodonfrost <diodon.frost@diodonfrost.me>
2019-05-10 21:43:23 +0200

    add openbsd support

--- tests/vagrant/vagrant_openbsd_target.rb
+++ tests/vagrant/vagrant_openbsd_target.rb
@@ -3,7 +3,7 @@ Vagrant.configure(2) do |config|
      OPENBSD_VERSION=`uname -a | awk -F " " '{print $3}'`
      wget -P /tmp https://ftp.openbsd.org/pub/OpenBSD/${OPENBSD_VERSION}/amd64/xbase64.tgz
      cd / && tar xzvphf /tmp/xbase64.tgz
-     pkg_add -z ansible
+     pkg_add -z ansible unzip-6
      ln -s /usr/local/bin/python2.7 /usr/bin/python
   SHELL
 end
