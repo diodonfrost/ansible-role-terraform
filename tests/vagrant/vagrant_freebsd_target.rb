10ca2bab192c7f5c04a9c53e9cd852e44129dcb0
diodonfrost <diodon.frost@diodonfrost.me>
2019-08-02 21:18:14 +0200

    fix kitchen-ci freebsd test

--- tests/vagrant/vagrant_freebsd_target.rb
+++ tests/vagrant/vagrant_freebsd_target.rb
@@ -3,5 +3,6 @@ Vagrant.configure(2) do |config|
     pkg install -y ncurses python gtar unzip
     pkg install -y py27-ansible
     ln -s /usr/local/bin/python2.7 /usr/bin/python
+    ln -s /usr/local/bin/ansible-playbook* /usr/local/bin/ansible-playbook
   SHELL
 end
